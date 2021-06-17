9534fc6986dd8a297d7a199d0bd1e38c0b45bb9a
Jain Bhavesh <bkjain339@gmail.com>
2021-06-17 19:22:14 +0530

    fb live url change

--- src/screens/FBLive/index.js
+++ src/screens/FBLive/index.js
@@ -3,15 +3,14 @@ import React from 'react';
 import { SafeAreaView, Image } from "react-native";
 import CustomeHeader from "../../components/CustomeHeader";
 import styles from "../../styles/style";
+import { WebView } from 'react-native-webview';
 
 const FBLive = (props) => {
     return (
         <View style={styles.secoundaryBG, styles.flex}>
             <SafeAreaView style={[styles.flex, styles.secoundaryBG]}>
                 <CustomeHeader headerText="Live Streaming" centerTextStyle={{ flex: 0.7, alignItems: "flex-start" }} onClick={() => props.navigation.goBack()} />
-                <View style={{ marginTop: 20 }}>
-                    <Image source={require('../../assets/stream.png')} style={{ width: "100%", height: "100%" }} />
-                </View>
+                <WebView source={{ uri: 'https://www.facebook.com/' }} />
             </SafeAreaView>
         </View>
     )
