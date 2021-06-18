3978e615a6e2f5acf1b2e587c8fec0d84aaa4291
Jain Bhavesh <bkjain339@gmail.com>
2021-06-18 18:22:07 +0530

    church live url added

--- src/screens/FBLive/index.js
+++ src/screens/FBLive/index.js
@@ -1,16 +1,44 @@
-import { Text, View } from "native-base";
-import React from 'react';
-import { SafeAreaView, Image } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ActivityIndicator, View } from "react-native";
 import CustomeHeader from "../../components/CustomeHeader";
 import styles from "../../styles/style";
 import { WebView } from 'react-native-webview';
+import { apiCalling } from "../../services/ApiCall";
+import { showToast } from "../../services/CommonFunction";
 
 const FBLive = (props) => {
+    const [url, setURL] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        geturl();
+    }, [])
+    const geturl = () => {
+        setLoading(true);
+
+        apiCalling("GET", "get-live-streaming", {}, "")
+            .then(res => res.json())
+            .then(response => {
+                setLoading(false);
+
+                if (response.success == true) {
+                    console.log('Api call result => ', response);
+                    setURL(response.data.acf.link);
+                } else
+                    showToast("Data not available");
+            }).catch((error) => {
+                console.log(error)
+                showToast(error)
+                setLoading(false);
+            });
+    }
     return (
         <View style={styles.secoundaryBG, styles.flex}>
             <SafeAreaView style={[styles.flex, styles.secoundaryBG]}>
                 <CustomeHeader headerText="Live Streaming" centerTextStyle={{ flex: 0.7, alignItems: "flex-start" }} onClick={() => props.navigation.goBack()} />
-                <WebView source={{ uri: 'https://www.facebook.com/' }} />
+                {loading ? <ActivityIndicator style={{ marginTop: 8 }} size="large" color="#460000" /> :
+                    <WebView source={{ uri: url }} />
+                }
             </SafeAreaView>
         </View>
     )
